e7b84c2710bcd67141498dbf98984fcb89e3b149
Huotes <aureliodosol@gmail.com>
2025-07-19 00:18:16 -0300

    remove referencias a weapon

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
 from pgzero.actor import Actor
 from pgzero.keyboard import keyboard
 from pygame import Rect
-from weapon import Weapon
 from config import *
+
 
 class Player:
     def __init__(self, pos):
@@ -12,12 +12,14 @@ class Player:
         self.anim_timer = 0
         self.facing_left = False
 
-        self.weapon = Weapon(self)
-
-        self.idle_images_right = [Actor(f"knight_f_idle_anim_f{i}") for i in range(4)]
-        self.idle_images_left  = [Actor(f"knight_f_idle_anim_f{i}_left") for i in range(4)]
-        self.run_images_right  = [Actor(f"knight_f_run_anim_f{i}") for i in range(4)]
-        self.run_images_left   = [Actor(f"knight_f_run_anim_f{i}_left") for i in range(4)]
+        self.idle_images_right = [Actor(f"wizzard_f_idle_anim_f{i}") for i in range(4)]
+        self.idle_images_left = [
+            Actor(f"wizzard_f_idle_anim_f{i}_left") for i in range(4)
+        ]
+        self.run_images_right = [Actor(f"wizzard_f_run_anim_f{i}") for i in range(4)]
+        self.run_images_left = [
+            Actor(f"wizzard_f_run_anim_f{i}_left") for i in range(4)
+        ]
         self.current_images = self.idle_images_right
 
     def update(self, dt, mouse_pos, mouse_buttons):
@@ -37,26 +39,22 @@ class Player:
         self.y += dy
 
         if dx != 0 or dy != 0:
-            self.current_images = self.run_images_left if self.facing_left else self.run_images_right
+            self.current_images = (
+                self.run_images_left if self.facing_left else self.run_images_right
+            )
         else:
-            self.current_images = self.idle_images_left if self.facing_left else self.idle_images_right
+            self.current_images = (
+                self.idle_images_left if self.facing_left else self.idle_images_right
+            )
 
         self.anim_timer += 1
         if self.anim_timer % 10 == 0:
             self.frame = (self.frame + 1) % len(self.current_images)
 
-        # Se botão esquerdo estiver pressionado, inicia ataque
-        if 1 in mouse_buttons:  # botão esquerdo está pressionado
-            self.weapon.start_attack()
-            
-        self.weapon.update(mouse_pos, dt)
-
     def draw(self):
         sprite = self.current_images[self.frame]
         sprite.pos = (self.x, self.y)
         sprite.draw()
-
-        self.weapon.draw()
 
     def get_rect(self):
         return Rect(self.x - 16, self.y - 16, 32, 32)
