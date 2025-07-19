1c832ae4890d78b68c3f6cb7f22cf4c721f208a0
Huotes <aureliodosol@gmail.com>
2025-07-19 00:14:02 -0300

    sobe o git ignore novo

--- main.py
+++ main.py
@@ -1,17 +1,52 @@
-import os
 import pgzrun
 from player import Player
-from config import *
 
-os.environ['PGZERO_ASSETS'] = 'assets'
+WIDTH = 800
+HEIGHT = 600
 
 player = Player((WIDTH // 2, HEIGHT // 2))
+mouse_pos = (0, 0)
+mouse_buttons = (False, False, False)
+
 
 def draw():
     screen.clear()
     player.draw()
 
+
 def update(dt):
-    player.update(dt)
+    player.update(dt, mouse_pos, mouse_buttons)
+
+
+def on_mouse_move(pos, rel, buttons):
+    global mouse_pos, mouse_buttons
+    mouse_pos = pos
+    # buttons é um set
+    mouse_buttons = (
+        1 in buttons,  # botão esquerdo
+        2 in buttons,  # botão meio
+        3 in buttons,  # botão direito
+    )
+
+
+def on_mouse_down(pos, button):
+    global mouse_buttons
+    # atualizar o estado do botão
+    if button == mouse.LEFT:
+        mouse_buttons = (True, mouse_buttons[1], mouse_buttons[2])
+
+
+def on_mouse_up(pos, button):
+    global mouse_buttons
+    if button == mouse.LEFT:
+        mouse_buttons = (False, mouse_buttons[1], mouse_buttons[2])
+
+
+def on_key_down(key):
+    if key == keys.K_1:
+        player.weapon.switch("1")
+    elif key == keys.K_2:
+        player.weapon.switch("2")
+
 
 pgzrun.go()
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from pgzero.actor import Actor
 from pgzero.keyboard import keyboard
 from pygame import Rect
+from weapon import Weapon
 from config import *
 
 class Player:
@@ -11,12 +12,15 @@ class Player:
         self.anim_timer = 0
         self.facing_left = False
 
-        # Carrega sprites
-        self.idle_images = [Actor(f"knight_f_idle_anim_f{i}") for i in range(4)]
-        self.run_images = [Actor(f"knight_f_run_anim_f{i}") for i in range(4)]
-        self.current_images = self.idle_images
+        self.weapon = Weapon(self)
 
-    def update(self, dt):
+        self.idle_images_right = [Actor(f"knight_f_idle_anim_f{i}") for i in range(4)]
+        self.idle_images_left  = [Actor(f"knight_f_idle_anim_f{i}_left") for i in range(4)]
+        self.run_images_right  = [Actor(f"knight_f_run_anim_f{i}") for i in range(4)]
+        self.run_images_left   = [Actor(f"knight_f_run_anim_f{i}_left") for i in range(4)]
+        self.current_images = self.idle_images_right
+
+    def update(self, dt, mouse_pos, mouse_buttons):
         dx, dy = 0, 0
         if keyboard.a:
             dx = -self.speed
@@ -32,21 +36,27 @@ class Player:
         self.x += dx
         self.y += dy
 
-        # Alterna animação
         if dx != 0 or dy != 0:
-            self.current_images = self.run_images
+            self.current_images = self.run_images_left if self.facing_left else self.run_images_right
         else:
-            self.current_images = self.idle_images
+            self.current_images = self.idle_images_left if self.facing_left else self.idle_images_right
 
         self.anim_timer += 1
         if self.anim_timer % 10 == 0:
             self.frame = (self.frame + 1) % len(self.current_images)
 
+        # Se botão esquerdo estiver pressionado, inicia ataque
+        if 1 in mouse_buttons:  # botão esquerdo está pressionado
+            self.weapon.start_attack()
+            
+        self.weapon.update(mouse_pos, dt)
+
     def draw(self):
         sprite = self.current_images[self.frame]
         sprite.pos = (self.x, self.y)
-        sprite.flip_x = self.facing_left
         sprite.draw()
+
+        self.weapon.draw()
 
     def get_rect(self):
         return Rect(self.x - 16, self.y - 16, 32, 32)
